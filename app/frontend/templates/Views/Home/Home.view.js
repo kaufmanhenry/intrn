a9d3e3cd457513e901848d9a371798ffe251313c
Henry Kaufman <hkaufman19@gmail.com>
2017-02-13 17:45:20 -0700

    Wrote some awesome filtering.

--- app/frontend/templates/Views/Home/Home.view.js
+++ app/frontend/templates/Views/Home/Home.view.js
@@ -3,15 +3,53 @@ angular.module('intrn')
         return {
             templateUrl: 'templates/Views/Home/Home.html',
             controller: ['$scope', '$q', 'Job', 'Enums', 'Error', function ($scope, $q, Job, Enums, Error) {
+                //Initiate Filters
+                $scope.filters = {
+                    roleFilters: [],
+                    locationFilters: [],
+                    jobFilters: []
+                };
+
                 $q.all([
                     Job.query(function (a) {
                         $scope.jobs = a;
+                        $scope.filteredJobs = a;
                     }).$promise,
                     Enums.get(function (a) {
                         $scope.enums = a;
                     }).$promise
                 ]).then(function () {
                 }, Error.handle);
+
+                $scope.setFilter = function (filter, filterText) {
+                    var index = $scope.filters[filterText].indexOf(filter.name);
+                    if (index >= 0) {
+                        $scope.filters[filterText].splice(index, 1);
+                    } else {
+                        $scope.filters[filterText].push(filter.name);
+                    }
+                    $scope.filterJobs();
+                };
+
+                $scope.filterJobs = function () {
+                    console.log($scope.filters);
+                    $scope.filteredJobs = $scope.jobs.filter(function (a) {
+                        if ($scope.filters.roleFilters.length > 0) {
+                            if ($scope.filters.roleFilters.indexOf(a.role.name) < 0) return;
+                        }
+                        if ($scope.filters.locationFilters.length > 0) {
+                            if ($scope.filters.locationFilters.indexOf(a.location.name) < 0) return;
+                        }
+                        if ($scope.filters.jobFilters.length > 0) {
+                            if ($scope.filters.jobFilters.indexOf(a.jobType.name) < 0) return;
+                        }
+                        return a;
+                    });
+                };
+
+                $scope.isInFilter = function (a, b) {
+                    return $scope.filters[a].indexOf(b.name) >= 0;
+                };
             }]
         };
     });
